749769c42b1b7d94f0a4d755805e40abb5946820
unknown <srrondo.hd@gmail.com>
2026-02-11 17:01:05 +0100

    Fix ModuleNotFoundError in Vercel

--- casapepe-backend/index.py
+++ casapepe-backend/index.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the current directory to sys.path to ensure local modules can be imported
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from server import create_app
 
 app = create_app()
